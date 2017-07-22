152f59ba46f8c73726fe66e9c29cbd87c9144dd2
Mark Steve Samson <hello@marksteve.com>
2017-07-22 17:51:18 +0800

    Add initial line chart

--- numberCardWithTrend/src/visual.ts
+++ numberCardWithTrend/src/visual.ts
@@ -54,26 +54,77 @@ module powerbi.extensibility.visual {
         private target: Selection<HTMLElement>;
         private host: IVisualHost;
         private settings: VisualSettings;
+        private cardTitle: Selection<HTMLElement>;
         private cardNumber: Selection<HTMLElement>;
+        private svg: Selection<HTMLElement>;
+        private chart: Selection<HTMLElement>;
+        private xAxis: Selection<HTMLElement>;
+        private yAxis: Selection<HTMLElement>;
+        private line: Selection<HTMLElement>;
 
         constructor(options: VisualConstructorOptions) {
             this.target = d3.select(options.element).append('div');
+            this.cardTitle = this.target.append('div').attr('class', 'card-title');
+            this.cardNumber = this.cardTitle.append('div').attr('class', 'card-number');
+            this.svg = this.target.append('svg');
+            this.chart = this.svg.append('g').attr('class', 'chart');
+            this.xAxis = this.chart.append('g')
+                .attr('class', 'x axis');
+            this.yAxis = this.chart.append('g')
+                .attr('class', 'y axis')
+                .attr('transform', 'translate(60, 30)');
+            this.line = this.chart.append('path')
+                .attr('transform', 'translate(60, 30)');
             this.host = options.host;
         }
 
         public update(options: VisualUpdateOptions) {
             try {
-                this.target.html(null);
-                let settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+                let dataView = options && options.dataViews && options.dataViews[0];
+                let settings = Visual.parseSettings(dataView);
                 let viewModel: ViewModel = visualTransform(options, this.host);
-                let cardNumber = this.target.append('div')
-                    .classed('card-number', true)
+
+                this.cardTitle.style('height', (options.viewport.height / 4) + 'px');
+
+                let cardNumber = this.cardNumber
                     .style('font-size', pixelConverterFromPoint(settings.cardNumber.fontSize))
                     .datum(viewModel.cardNumber);
                 cardNumber.text(function (d) {
                     return compactInteger(d.value);
                 });
-            } catch (e) {
+
+                let width = options.viewport.width - 60;
+                let height = options.viewport.height * 3 / 4 - 30 - 30;
+                this.svg.attr('width', width + 60);
+                this.svg.attr('height', height + 30 + 30);
+
+                let x = d3.time.scale().range([0, width]);
+                let y = d3.scale.linear().range([height, 0]);
+                let xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(5);
+                let yAxis = d3.svg.axis().scale(y).orient('left').ticks(5)
+                    .tickSize(-width)
+                    .tickFormat(function (d) { return compactInteger(d); });
+                var line = d3.svg.line()
+                    .x(function (d: any) { return x(d.date); })
+                    .y(function (d: any) { return y(d.value); });
+
+                let dates = dataView.categorical.categories[0].values;
+                let values = dataView.categorical.values[0].values;
+                let data = dates.map(function (d, i) {
+                    return {
+                        date: d,
+                        value: values[i]
+                    };
+                });
+                x.domain(d3.extent(data, function (d: any) { return d.date; }));
+                y.domain(d3.extent(data, function (d: any) { return d.value; }));
+
+                this.xAxis.attr('transform', 'translate(60, ' + (height + 30) + ')')
+                    .call(xAxis);
+                this.yAxis.call(yAxis);
+                this.line.datum(data).attr('d', <any>line);
+
+						} catch (e) {
                 console.error(e);
                 throw e;
             }
